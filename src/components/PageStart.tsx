a9710b2449a49d9917192982c84832012173c57b
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-09 10:59:56 +0100

    have map

--- src/components/PageStart.tsx
+++ src/components/PageStart.tsx
@@ -1,7 +1,11 @@
+import europeanMap from '../assets/images/map.jpg';
+
 export const PageStart = () => {
         return (
             <>
                 <p>Welcome to this travel info site.</p>
+            
+                <img src={europeanMap}/>
             </>
         );
     };
